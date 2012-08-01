3ebcd78446accfd4ca9c678ac7973f0a8909a5a1
tanitanin <tajima2210@gmail.com>
2012-08-01 14:48:29 +0900

    pthreadバージョン

--- echo-server/src/main.c
+++ echo-server/src/main.c
@@ -5,9 +5,13 @@
 #include <sys/socket.h> // socket,bind,listen,accept,recv
 #include <netinet/in.h> // htons
 #include <unistd.h>
+#include <pthread.h>
 
 #define BUFSIZE 1024
 #define PORT 40320
+
+int server_socket(void);
+int accept_loop(int);
 
 // socket bind and listen
 int server_socket(void)
@@ -45,42 +49,59 @@ int accept_loop(int listen_fd)
 {
   int conn_fd;
   struct sockaddr_in caddr; // address for client
-  int len = sizeof(caddr);
-  int rsize;
-  char buf[BUFSIZE];
-  int pid;
-
+  socklen_t len;
+  pthread_t tid;
+  void process(void *);
+  
   while(1) {
-    len = sizeof(caddr);
+    len = (socklen_t) sizeof(caddr);
     
     // connection
     if((conn_fd = accept(listen_fd,(struct sockaddr *)&caddr,&len)) < 0) {
       perror("accept");
-      exit(EXIT_FAILURE);
+      return -1;
     }
+    printf("Connected.\n");
     
-    if((pid = fork()) == 0) {
-      // read
-      while(rsize = read(conn_fd, buf, BUFSIZE)) {
-        write(conn_fd, buf, rsize);
-      }
-      close(conn_fd);
-      exit(0);
+    if(pthread_create(&tid,NULL,&process,(void *)conn_fd)!=0) {
+      perror("thread create");
+      return -2;
     }
-    close(conn_fd);
   }
 }
 
+// processing after accepted
+void process(void *arg)
+{
+  printf("process start.\n");
+  // thread detach
+  pthread_detach(pthread_self());
+  
+  int conn_fd = (int) arg;
+  int rsize;
+  char buf[BUFSIZE];
+  
+  // read
+  while(rsize = read(conn_fd, buf, BUFSIZE)) {
+    write(conn_fd, buf, rsize);
+  }
+  close(conn_fd);
+  pthread_exit((void *)0);
+}
+
+// main function
 int main(int argc, char **argv)
 {
   int listen_fd;
-
+  
   if((listen_fd = server_socket())<0) {
     perror("socket");
     exit(EXIT_FAILURE);
   }
+  printf("Socket generated.\n");
   
   accept_loop(listen_fd);
+  printf("accept_loop ended.\n");
   
   close(listen_fd);
   return 0;
